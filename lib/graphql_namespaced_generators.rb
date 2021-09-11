650c0776289858cc1b192afd78dd75e7e7b01d60
Anton Radushev <doromones@gmail.com>
2021-09-11 13:09:05 +0300

    init

---
+++ lib/graphql_namespaced_generators.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "graphql_namespaced_generators/version"
+
+module GraphqlNamespacedGenerators
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/graphql_namespaced_generators/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GraphqlNamespacedGenerators
+  VERSION = "0.1.0"
+end
